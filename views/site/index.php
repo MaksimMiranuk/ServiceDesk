b612818ef1c69710536e662622e8ced179a725b3
MaksimMiranuk <92392700+MaksimMiranuk@users.noreply.github.com>
2021-10-12 20:16:15 +0300

    Add files via upload

---
+++ views/site/index.php
@@ -0,0 +1,23 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Программное средство реализации Service Desk решения
+для компаний малого бизнес-сегмента</h1>
+
+<?php echo CHtml::link('Справочники',array('/site/dict')); ?>
+<div></div>
+<?php echo CHtml::link('Склад',array('/site/store')); ?>
+<div></div>
+<?php echo CHtml::link('О программе',array('/site/page', 'view'=>'about')); ?>
+<div></div>
+<?php echo CHtml::link('Контакты',array('site/contact')); ?>
+<div></div>
+<?php 
+    if (Yii::app()->user->isGuest)
+        echo CHtml::link('Вход',array('site/login'));
+    else
+        echo CHtml::link('Выход ('.Yii::app()->user->name.')',array('site/logout'));
+?>
